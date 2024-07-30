0d92e1b6b259ec6bbd72ad025f0be41708df1937
bedri.keskin <bedri.keskin@gmail.com>
2024-07-31 01:33:14 +0300

    Gaia binaries are predicted by Fourier coeffs

---
+++ Gaia/FourierCoeffs_Gaia_edit.py
@@ -0,0 +1,43 @@
+# FourierCoeffs_Gaia.csv'nin sonundaki Villanova ID,morph,Villanova Class sütunlarını doldurur
+
+import pandas as pd
+import querySimbad
+
+Kepler = pd.read_csv("/Users/tiga/Documents/EclipsingBinaryClassification/Kepler/FourierCoeffs_Kepler.csv",
+                     delim_whitespace=False, index_col=False)
+Tess = pd.read_csv("/Users/tiga/Documents/EclipsingBinaryClassification/Tess/FourierCoeffs_TESS.csv",
+                   delim_whitespace=False, index_col=False)
+file = '/Users/tiga/Documents/EclipsingBinaryClassification/Gaia/FourierCoeffs_Gaia.csv'
+Gaia = pd.read_csv(file, delim_whitespace=False, index_col=False)
+
+for index, gaia in Gaia.iterrows():
+    KICandTIC = querySimbad.SimbadQuery_GaiaID2_KICandTIC('Gaia DR3 ' + str(gaia['ID']))
+    KIC = KICandTIC[0]
+    TIC = KICandTIC[1]
+    print(f"index {index} Gaia ID: {gaia['ID']} KIC: {KIC} TIC: {TIC}")
+
+    kepler = pd.DataFrame()
+    tess = pd.DataFrame()
+
+    if KIC is not None:
+        kepler = Kepler.loc[Kepler['ID'] == KIC]
+
+    if TIC is not None:
+        tess = Tess.loc[Tess['ID'] == TIC]
+
+    if not kepler.empty:
+        print(f"kepler {kepler}")
+        gaia['Villanova ID'] = KIC
+        gaia['morph'] = kepler['morph']
+        gaia['Villanova Class'] = kepler['label']
+        Gaia.loc[index] = gaia
+    elif not tess.empty:
+        print(f"tess {tess}")
+        gaia['Villanova ID'] = TIC
+        gaia['morph'] = tess['morph']
+        gaia['Villanova Class'] = tess['label']
+        Gaia.loc[index] = gaia
+    else:
+        print(f"Error. Gaia ID {gaia['ID']} KIC {KIC} TIC {TIC}. kepler {kepler} tess {tess}.")
+
+Gaia.to_csv(file, index=False)
--- querySimbad.py
+++ querySimbad.py
@@ -19,8 +19,56 @@ def SimbadQueryID(fromID, toID):
         return None
 
 
-kicID = SimbadQueryID("TIC 0120684604", 'KIC')
+def SimbadQuery_GaiaID2_KICandTIC(fromID):
+    table = Simbad.query_objectids(fromID)
+    if table is not None:
+        string = table['ID'].astype(str)
+        table.replace_column('ID', string)
+        data = table['ID'].data
+        arrayKIC = np.flatnonzero(np.core.defchararray.find(data, 'KIC') != -1)
+        arrayTIC = np.flatnonzero(np.core.defchararray.find(data, 'TIC') != -1)
+
+        KIC = None
+        TIC = None
+
+        if len(arrayKIC) == 1:
+            KIC = data[arrayKIC[0]].split()[-1]
+            if len(KIC) == 7:
+                KIC = "KIC0" + KIC
+            elif len(KIC) == 8:
+                KIC = "KIC" + KIC
+            else:
+                print(f"Length of KIC {KIC} is other than 7 or 8. Length: {len(KIC)}")
+
+        if len(arrayTIC) == 1:
+            TIC = data[arrayTIC[0]].split()[-1]
+            if len(TIC) == 7:
+                TIC = "TIC 000" + TIC
+            if len(TIC) == 8:
+                TIC = "TIC 00" + TIC
+            elif len(TIC) == 9:
+                TIC = "TIC 0" + TIC
+            elif len(TIC) == 10:
+                TIC = "TIC " + TIC
+            else:
+                print(f"Length of TIC {TIC} is other than 8 or 9 or 10. Length: {len(TIC)}")
+
+        return KIC, TIC
+    else:
+        return None, None
+
+
+'''
+kicID = SimbadQuery_GaiaID2_KICandTIC("Gaia DR3 6384729641558823040")
 if kicID is not None:
     print(kicID)
 else:
     print("not found")
+'''
+'''
+kicID = SimbadQueryID("Gaia DR3 2102813720380137600", 'KIC')
+if kicID is not None:
+    print(kicID)
+else:
+    print("not found")
+'''
