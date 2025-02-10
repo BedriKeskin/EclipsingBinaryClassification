d7c2aa4a4667a24d7984ef77ab3aa2fb8d50056e
Bedri Keskin <>
2025-02-10 20:22:39 +0300

    tess

--- Kepler/runMultiple.py
+++ Kepler/runMultiple.py
@@ -13,7 +13,7 @@ def run_starshadow_py():
 
     LCdatas = glob.glob("./" + folder + "/*.txt")
 
-    for index, LCdata in enumerate(LCdatas[1570:1575]):
+    for index, LCdata in enumerate(LCdatas[1575:1580]):
         print("\n", index, LCdata, len(LCdata))
 
         try:
