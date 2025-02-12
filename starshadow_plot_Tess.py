900890b0c7dd99c3c683fd23fe4179b4de90742f
Bedri Keskin <>
2025-02-13 02:00:40 +0300

    kepler start shadow

--- starshadow_plot_Tess.py
+++ starshadow_plot_Tess.py
@@ -3,18 +3,21 @@ import glob
 import os
 
 
-folder = "./Tess/"
+folder = "./Kepler/"
 LCdatas = glob.glob(folder+"StarShadow/*.txt")
 
 for index, LCdata in enumerate(LCdatas):
     print("\n", index, LCdata)
 
     try:
-        if not os.path.exists(folder + "StarShadowAnalysis/" + os.path.basename(LCdata)[:-4] + "_analysis/" + os.path.basename(LCdata)[:-4] + "_eclipse_analysis_derivatives_h.png_ModelOnly.png"):
-            sts.ut.plot_all_from_file(LCdata, load_dir=folder+"StarShadowAnalysis", save_dir=folder+"StarShadowAnalysis", show=False)
-            print("plotted")
-        else:
-            print("already plotted")
+        # if not os.path.exists(folder + "StarShadowAnalysis/" + os.path.basename(LCdata)[:-4] + "_analysis/" + os.path.basename(LCdata)[:-4] + "_analysis_summary.csv"):
+        #     print("Analysis not done yet ", os.path.basename(LCdata)[:-4])
+        # else:
+            if not os.path.exists(folder + "StarShadowAnalysis/" + os.path.basename(LCdata)[:-4] + "_analysis/" + os.path.basename(LCdata)[:-4] + "_eclipse_analysis_derivatives_h.png_ModelOnly.png"):
+                sts.ut.plot_all_from_file(LCdata, load_dir=folder+"StarShadowAnalysis", save_dir=folder+"StarShadowAnalysis", show=False)
+                print("newly plotted")
+            else:
+                print("already plotted")
     
     except Exception as e:
         print(f"{e} Error")
